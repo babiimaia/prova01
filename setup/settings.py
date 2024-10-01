a98a55b9a29304ee5ed8189ad2e02e52e381b07d
Barbara Maia <babiimaia6@gmail.com>
2024-10-01 20:18:51 -0300

    criando um app

--- setup/settings.py
+++ setup/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.prova01',
+    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
