a01fdbb152722b5161638f369800776c3cf94da8
Barbara Maia <babiimaia6@gmail.com>
2024-10-01 20:55:42 -0300

    Tendando sem sucesso

--- setup/urls.py
+++ setup/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from todos.views import home
+path('', home)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- todos/apps.py
+++ todos/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
-class TodosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todos'
+class Todo(models.Model):
+title = models.CharField(max_length=100, null=False, blank=False)
+created_at =models.DateField(auto_now_add=True,null=False,blank=False)
+deadline = models.DateField(null=False, blank=False)
+finished_at = models.DateField(null=True)
